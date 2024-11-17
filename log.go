6ca030488eba79c6b527d9dea79d29891925cedd
HowXu <howxu366@outlook.com>
2024-11-17 11:28:30 +0800

    fix: go 1.18 bug

--- log.go
+++ log.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var GLOBAL_DEBUG = true
 
-func log(format string, a ...any) (n int, err error) {
+func log(format string, a ...interface{}) (n int, err error) {
 	if GLOBAL_DEBUG {
 		return fmt.Printf(format,a...)
 	}
